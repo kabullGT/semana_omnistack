50b46430c5ce88351fecee5ddfaafad1b0907c5b
kabull_gt <kabull@royalrockets.com>
2020-04-02 21:45:36 -0300

    Front end do App.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+import './global.css'
+
+// JSX: Java and HTML when HTML is inside Javascript
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Kabull by Omnistack...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<Header title="Semana Omnistack"/>
+    <Routes></Routes>
   );
 }
 
